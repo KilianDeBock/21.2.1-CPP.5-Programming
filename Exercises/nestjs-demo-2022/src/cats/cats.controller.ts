e713db22a994f27e7d02d4f030b2adbba8cacb64
Kilian De Bock <91065793+KilianDeBock@users.noreply.github.com>
2022-10-06 15:11:49 +0200

    22.40.4.2: Update app

--- Exercises/nestjs-demo-2022/src/cats/cats.controller.ts
+++ Exercises/nestjs-demo-2022/src/cats/cats.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  ImATeapotException,
+  Param,
+  ParseIntPipe,
+  Post,
+} from '@nestjs/common';
 import { CreateCatDto } from './dto/create-cat.dto';
 import { CatsService } from './cats.service';
 import { Cat } from '../interfaces';
@@ -8,7 +16,7 @@ export class CatsController {
   constructor(private catsService: CatsService) {}
 
   @Get('cat/:id')
-  findOne(@Param('id') id: string): string {
+  findOne(@Param('id', ParseIntPipe) id: number): string {
     return `This action returns a #${id} cat`;
   }
 
@@ -21,6 +29,8 @@ export class CatsController {
   @Post('createCat')
   // @HttpCode(204)
   async createCat(@Body() createCatDto: CreateCatDto): Promise<string> {
+    // throw new HttpException('Forbidden', HttpStatus.I_AM_A_TEAPOT);
+    throw new ImATeapotException();
     this.catsService.create(createCatDto);
     console.log(createCatDto);
     return 'This action adds a new cat';
--- Exercises/nestjs-demo-2022/src/cats/dto/create-cat.dto.ts
+++ Exercises/nestjs-demo-2022/src/cats/dto/create-cat.dto.ts
@@ -1,5 +1,13 @@
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
 export class CreateCatDto {
+  @IsString()
+  @IsNotEmpty()
   name: string;
+
+  @IsNumber()
   age: number;
+
+  @IsString()
   breed: string;
 }
